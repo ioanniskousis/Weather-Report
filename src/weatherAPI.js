8f7f36941e7175428726109ef69a7e2c85defd94
Ioannis Kousis (John) <bonobo@otenet.gr>
2020-08-20 19:15:42 +0300

    Update weatherAPI.js

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -10,7 +10,7 @@ async function fetchCityWeather(url, callBack, loadStartTime) {
 function downloadCityData(cityCode, callBack) {
   const loadStartTime = Date.now();
   const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=';
-  const apiCode = '&appid=a832f5712931bcd07786d24b4290543e';
+  const apiCode = '&appid=<api-key>';
   const url = weatherURL.concat(cityCode).concat(apiCode);
   fetchCityWeather(url, callBack, loadStartTime);
 }
